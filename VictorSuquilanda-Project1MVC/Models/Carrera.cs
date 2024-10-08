4ad77ab39058cd4ad618914272669016d2d6c084
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-10-08 15:09:20 -0500

    Commit 1: Clase 2

---
+++ VictorSuquilanda-Project1MVC/Models/Carrera.cs
@@ -0,0 +1,7 @@
+﻿namespace VictorSuquilanda_Project1MVC.Models
+{
+    public class Carrera
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ VictorSuquilanda-Project1MVC/Models/EstudianteUdla.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VictorSuquilanda_Project1MVC.Models
+{
+    public class EstudianteUdla
+    {
+        public string IdBanner { get; set; }
+        [AllowNull]
+        [Required]
+        [MaxLength(50)]
+        public string? Nombre { get; set; }  // Al emplar '?' junto con [AllowNull] le permitimos a este dato tener valor Null  
+        public string Correo { get; set; }
+        public Carrera Carrera { get; set; }
+    }
+}
