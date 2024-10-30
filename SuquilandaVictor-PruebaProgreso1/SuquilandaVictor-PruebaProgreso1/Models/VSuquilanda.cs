cdcf90411658a06c0fd4d88f89e74c8e956a778b
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-10-30 17:35:01 -0500

    Commit3: Se añadió la imagen y el botón con la funcionalidad de la alerta. Estas modificaciones fueron realizadas con css y js. Además se cambio el estilo de las tablas y vistas del CRUD.

--- SuquilandaVictor-PruebaProgreso1/SuquilandaVictor-PruebaProgreso1/Models/VSuquilanda.cs
+++ SuquilandaVictor-PruebaProgreso1/SuquilandaVictor-PruebaProgreso1/Models/VSuquilanda.cs
@@ -11,7 +11,6 @@ namespace SuquilandaVictor_PruebaProgreso1.Models
         public string Name { get; set; }
         [Range(0,100)]
         public int Edad { get; set; }
-        [Range(0, 100000)]
         public double Fondos { get; set; }
         [EmailAddress]
         public string Correo {  get; set; }
--- SuquilandaVictor-PruebaProgreso1/SuquilandaVictor-PruebaProgreso1/wwwroot/js/site.js
+++ SuquilandaVictor-PruebaProgreso1/SuquilandaVictor-PruebaProgreso1/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function alerta() {
+    alert("Mi nombre es Víctor Suquilanda.\nMis hobbies son la música, los videojuegos y a veces cocinar");
+}
